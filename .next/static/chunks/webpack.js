20a1dd1d5126bb14badba256108061e67214b24f
ww2kvn <ww2kvn@Immanuels-MacBook-Pro.local>
2022-02-12 23:52:44 -0600

    added git ignore file

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "93767723f94a4b54"; }
+/******/ 		__webpack_require__.h = function() { return "cd6e77d90653fe53"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
