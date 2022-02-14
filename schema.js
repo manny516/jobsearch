a661f2dd888d4cd919bafba738bfb58396acee5a
ww2kvn <ww2kvn@Immanuels-MacBook-Pro.local>
2022-02-14 03:21:44 -0600

    updated schema

--- schema.js
+++ schema.js
@@ -1,22 +1,44 @@
-const { GraphQLObjectType} = require("graphql");
+const axios = require('axios');
+const { 
+    GraphQLObjectType, 
+    GraphQLInt,
+    GraphQLString, 
+    GraphQLBoolean,
+    GraphQLList
+} = require("graphql");
 
 const JobInfo = new GraphQLObjectType({
     name: 'JobInfo',
-
     fields: ()=>({
+        id : { type : GraphQLInt },
+        company : { type : GraphQLString },
+        position : { type : GraphQLString }, 
+        location : { type : GraphQLString },
+        view_count : {type : GraphQLInt},
+        time_posted : {type : GraphQLString} ,
+        applied_count : {type : GraphQLInt},
+        bookmark : {type : GraphQLBoolean} ,
+        team_type : {type : GraphQLString},
+        salary : {type : GraphQLString},
+        min_qual : {type : GraphQLString},
+        prefer_qual : {type : GraphQLString},
+        description : {type : GraphQLString}
 
     })
-})
+});
 
-// const schema = new GraphQLSchema({
-//     query: new GraphQLObjectType({
-//         name : "Hello world",
-//         fields: () =>({
-//             message : { 
-//                 type: GraphQLString,
-//                 resolve: () => 'Hello world'
-//             }
-//         })
-//     })
-//     }
-// );
+
+const RootQuery = new GraphQLObjectType({
+
+    name : 'RootQuery',
+    fields : {
+        jobinfo : {
+            type : new GraphQLList(JobInfoType),
+            resolve(parent,args){
+                return axios.get();
+            }
+        }
+    }
+
+
+})
--- server.js
+++ server.js
@@ -1,13 +1,22 @@
 const express =  require("express");
 const {graphqlHTTP} = require("express-graphql");
-const schema = require("./schema");
+const fs = require('fs');
+const path = require('path');
+// const schema = require("./schema");
 
 const app = express();
 
-app.use("/graphql", graphqlHTTP({
-    schema,
-    graphiql : true
-}))
+// app.use("/graphql", graphqlHTTP({
+//     schema,
+//     graphiql : true
+// }))
+
+app.get("/api/jobsearch",(req,res) =>{
+    let rawData = fs.readFileSync(path.resolve(__dirname,'jobsearch.json'));
+    let jsonParse = JSON.parse(rawData);
+    res.send(jsonParse);
+
+})
 
 const PORT = process.env.PORT || 4000;
 
