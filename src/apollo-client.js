81e7ca70be48859c0294bd0dcfa4ded8aa7b637f
ww2kvn <ww2kvn@Immanuels-MacBook-Pro.local>
2022-02-19 15:57:22 -0600

    removed duplicate files

--- src/apollo-client.js
+++ src/apollo-client.js
@@ -2,7 +2,7 @@
 import { ApolloClient, InMemoryCache } from "@apollo/client";
 
 const client = new ApolloClient({
-    uri: "https://api.graphql.jobs/",
+    uri: "http://localhost:4000/graphql",
     cache: new InMemoryCache(),
 });
 
