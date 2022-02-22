a0e4e0bd74521dae6b0b5fcddcf80a10ca269318
ww2kvn <ww2kvn@Immanuels-MacBook-Pro.local>
2022-02-21 23:58:56 -0600

    seprated job items into seperate Component / added a interface component

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,34 +2,9 @@ import Head from 'next/head';
 import { gql } from "@apollo/client";
 import client from "../apollo-client";
 import { Key, ReactChild, ReactFragment, ReactPortal } from 'react';
-import {FaRegCommentDots,FaBookmark,FaInfoCircle,FaEye,FaMapMarker,FaCircle} from "react-icons/fa";
+import {FaRegCommentDots} from "react-icons/fa";
+import Jobitem from '../comp/Jobitems';
 
-export async function getStaticProps() {
-    const { data } = await client.query({
-      query: gql`
-        query JobInfo {
-          job{
-            id
-            company
-            position
-            location
-            view_count
-            time_posted
-            applied_count
-            bookmark
-            team_type
-            salary   
-          }
-        }
-      `,
-    });
-
-    return {
-      props: {
-        job: data.job,
-      },
-   };
-}
 
 export default function Home({job} : any) {
   return (
@@ -42,38 +17,7 @@ export default function Home({job} : any) {
              </form>
            </header>
         </article>
-            {/* <JobSearches jobData={job} /> */}
-           {job.map((job: Jobinfo) => (
-            <article key={job.id} className="border-solid border-2 m-2 flex flex-row results-job-search bg-neutral-50  rounded-lg p-8">
-
-              <article className="m-0 p-4 basis-9/12">
-
-                    <blockquote className="w-20 border-solid border-2 float-left h-full mr-4 rounded-lg ">
-                      <img src="" alt="" />
-                    </blockquote>
-                    <h2> {job.company}</h2>
-                    <p className=" text-indigo-300"> {job.position}</p>
-                    <blockquote className='flex text-slate-500'>
-                      <span className='text-xs '><FaMapMarker className='inline-block  mr-1'/>{job.location}</span>  
-                      <span className='ml-6 pr-2'><FaEye className='inline-block mr-1'/>{job.view_count} </span> 
-                    </blockquote>
-                    <blockquote className=' flex text-slate-500 gap-4'>
-                      <span> {job.time_posted}</span> 
-                      <span><FaCircle className='inline-block  mr-1'/> position</span> 
-                      <span><FaCircle className='inline-block  mr-1 text-xs' /> {job.applied_count}</span>
-                    </blockquote>
-                </article>
-                
-                <article className="basis-3/12 text-slate-500 ">
-                    <blockquote className='flex flex-row gap-2 justify-end pb-4 text-stone-400'>
-                      <span data-attribute={job.bookmark}><FaBookmark /></span> <span><FaInfoCircle/> </span>
-                    </blockquote>
-                    <p className='text-xs'>{job.team_type}</p>
-                    <span><strong className=' text-slate-900'>{job.salary}</strong> / year</span>
-                </article>
-
-              </article>
-          ))}
+        <Jobitem jobData={job} />
         </section>
 
         <section className="jb-side-bar basis-2/5 py-10">
@@ -129,18 +73,29 @@ export default function Home({job} : any) {
   )
 }
 
-interface Jobinfo{
-  id : number,
-  company : string,
-  position : string,
-  location : string,
-  view_count : number,
-  time_posted : string,
-  applied_count : number,
-  bookmark : boolean,
-  team_type: string,
-  salary: string,
-  min_qual : Array<string>,
-  prefer_qual : Array<string>,
-  desciption : string 
-}
+export async function getStaticProps() {
+    const { data } = await client.query({
+      query: gql`
+        query JobInfo {
+          job{
+            id
+            company
+            position
+            location
+            view_count
+            time_posted
+            applied_count
+            bookmark
+            team_type
+            salary   
+          }
+        }
+      `,
+    });
+
+    return {
+      props: {
+        job: data.job,
+      },
+   }
+  }
