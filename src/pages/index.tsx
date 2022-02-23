657c107a89cbb11f4e04bfae45541cb073b6bce7
ww2kvn <ww2kvn@Immanuels-MacBook-Pro.local>
2022-02-23 08:03:39 -0600

    fixed and merged updated refactored script

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,43 +2,9 @@ import Head from 'next/head';
 import { gql,useQuery } from "@apollo/client";
 import client from "../apollo-client";
 import { Key, ReactChild, ReactFragment, ReactPortal } from 'react';
-<<<<<<< HEAD
-import {FaRegCommentDots,FaBookmark,FaInfoCircle,FaEye,FaMapMarker,FaCircle} from "react-icons/fa";
 import Jobitem from '../comps/Jobitems';
-=======
-import {FaRegCommentDots} from "react-icons/fa";
-import Jobitem from '../comp/Jobitems';
-import JobFullDesc from '../comp/JobFullDesc';
+import JobFullDesc from '../comps/JobFullDesc';
 
-
->>>>>>> 941f3c03411b493fb78c9b014e1c839b45739b10
-
-export async function getStaticProps() {
-    const { data } = await client.query({
-      query: gql`
-        query JobInfo {
-          job{
-            id
-            company
-            position
-            location
-            view_count
-            time_posted
-            applied_count
-            bookmark
-            team_type
-            salary   
-          }
-        }
-      `,
-    });
-
-    return {
-      props: {
-        job: data.job,
-      },
-   };
-}
 
 export default function Home({job} : any) {
 
@@ -52,11 +18,7 @@ export default function Home({job} : any) {
              </form>
            </header>
         </article>
-<<<<<<< HEAD
-            <Jobitem jobData={job} />
-=======
           <Jobitem jobData={job} />
->>>>>>> 941f3c03411b493fb78c9b014e1c839b45739b10
         </section>
 
         <section className="jb-side-bar basis-2/5 py-10">
@@ -67,23 +29,6 @@ export default function Home({job} : any) {
   )
 }
 
-<<<<<<< HEAD
-interface Jobinfo{
-  id : number,
-  company : string,
-  position : string,
-  location : string,
-  view_count : number,
-  time_posted : string,
-  applied_count : number,
-  bookmark : boolean,
-  team_type: string,
-  salary: string,
-  min_qual : Array<string>,
-  prefer_qual : Array<string>,
-  desciption : string 
-}
-=======
 export async function getStaticProps() {
 
     const  jobitems  = (await client.query({
@@ -114,4 +59,3 @@ export async function getStaticProps() {
    }
 
   }
->>>>>>> 941f3c03411b493fb78c9b014e1c839b45739b10
