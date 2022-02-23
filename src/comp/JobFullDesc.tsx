359cea7670ac2515e873fbfcbc6df7545b5b5c71
ww2kvn <ww2kvn@Immanuels-MacBook-Pro.local>
2022-02-23 07:47:50 -0600

    added all data to job Description

--- src/comp/JobFullDesc.tsx
+++ src/comp/JobFullDesc.tsx
@@ -6,73 +6,80 @@ import { gql, useQuery } from '@apollo/client';
           query JobFullData {
               job{
                 id
-                salary
-                time_posted
-                min_qual
-                description
                 company
+                min_qual
+                prefer_qual
+                location
+                description
               }
           }
         `;
 
 export default function JobFullDesc( ){
+
+   
+    const router = useRouter();
+    const { pid } = router.query;
+
     const{loading,error,data} = useQuery(jobDesc);
     const JobDescResult = {data};
-    const router = useRouter();
-      const { pid } = router.query;
-    if(loading == false){
-       const Results = JobDescResult.data.job;
-       if(pid === Results[1].id){
-        console.log(`Page id${pid}`)
-      }
+
+    if (loading) {
+      return <h2>Loading...</h2>;
     }
-      
-    return (
+
+    if (error) {
+      console.error(error);
+      return null;
+    }
+
+    const Results = JobDescResult.data.job;
     
-        <article className="px-10 ">
-           <h2> Job results : {pid} </h2>
+    return (
+     
+        Results.filter((jDesc : any) => jDesc.id == pid)
+        .map((jDesc : any) =>(
+
+             <article key={jDesc.id} className="px-10 ">
+              <h2> Job results</h2>
 
 
-           <article className="text-center">
-             <img src="" alt="" className="w-60 h-60 m-auto border-solid border-2 rounded-lg" />
-             <h2> Job info </h2>
-             <p>Job subtitle information</p>
-           </article>
+              <article className="text-center">
+                <img src="" alt="" className="w-60 h-60 m-auto border-solid border-2 rounded-lg" />
+                <h2> {jDesc.company} </h2>
+                <p>{jDesc.location}</p>
+              </article>
 
-           <article className=" mt-10 border-t-2 border-black">
-             <h3>Title 2</h3>
-             <ul className="list-disc pl-8">
-               <li>Pellentesque eleifend nisl nec leo porttitor consequat.</li>
-               <li>Quisque vehicula odio vel porttitor maximus.</li>
-               <li>Vivamus eleifend dui at ullamcorper feugiat.</li>
-             </ul>
-           </article>
+              <article className=" mt-10 border-t-2 border-black">
+                <h3>Minimum qualifications:</h3>
+                <ul className="list-disc pl-8">
+                  {jDesc.min_qual}
+                </ul>
+              </article>
 
-           <article className="mt-10">
-             <h3>Title 3</h3>
-             <ul className="list-disc pl-8">
-               <li>Pellentesque eleifend nisl nec leo porttitor consequat.</li>
-               <li>Quisque vehicula odio vel porttitor maximus.</li>
-              </ul>
-           </article>
+              <article className="mt-10">
+                <h3>Preferred qualifications:</h3>
+                <ul className="list-disc pl-8">
+                   {jDesc.prefer_qual}
+                  </ul>
+              </article>
 
-           <article className=" mt-10 border-t-2 border-black">
-             <h3>About Company</h3>
-             <p>
-               Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-               Vivamus non mi condimentum, scelerisque turpis eu, blandit diam.
-               Duis aliquam ultricies ante. Fusce sed diam congue, suscipit urna vel,
-               elementum lorem. Vivamus mattis nisi sed auctor eleifend. Donec feugiat
-               nec nunc eget sagittis. Pellentesque nec rhoncus diam. Orci varius natoque
-               penatibus et magnis dis parturient montes, nascetur ridiculus mus.
-             </p>
-           </article>
+              <article className=" mt-10 border-t-2 border-black">
+                <h3>About Company</h3>
+                <p>
+                 {jDesc.description}
+                </p>
+              </article>
 
-           <article className="mt-10 pt-10 flex gap-2">
-             <button className="border-solid border-2 rounded-lg p-3 bg-indigo-500 text-white basis-1/2"> Apply now</button> <button className="border-solid border-2   text-3xl p-3 rounded-lg bg-grey-purple text-white"> <FaRegCommentDots/> </button>
-           </article>
+              <article className="mt-10 pt-10 flex gap-2">
+                <button className="border-solid border-2 rounded-lg p-3 bg-indigo-500 text-white basis-1/2"> Apply now</button> <button className="border-solid border-2   text-3xl p-3 rounded-lg bg-grey-purple text-white"> <FaRegCommentDots/> </button>
+              </article>
 
-         </article>
+            </article>
+
+
+
+        ))
 
     )
 
